d178a44e196505bc15383da7f9935fcd2d5eb3b8
Gian Domenico <giando64f95@gmail.com>
2024-12-28 19:37:58 +0100

    translate e layout

--- js/translation.js
+++ js/translation.js
@@ -10,5 +10,12 @@ const translations = {
     description: "The counter is a key element in computing. It can be either a hardware part, made with simple Flip-Flops, or a software part, like the one below. This is created and animated with JavaScript!",
     increment: "+",
     decrement: "-"
-    }
+    },
+    es: {
+        title: "¡El Contador!",
+        description: "El contador - <i>counter</i> en inglés - es un elemento clave en la informática. Puede ser tanto una parte de hardware, realizada con simples Flip-Flops, como una parte de software como el siguiente: creado y animado con JavaScript!",
+        increment: "+",
+        decrement: "-"
+    },
+    
 };
